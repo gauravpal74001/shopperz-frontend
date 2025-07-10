c3965370f1be3a3de884c59255d461d5de655eb6
gauravpal74001 <palg74001@gmail.com>
2025-07-10 14:53:29 +0530

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'mern-balancer-1949767843.ap-southeast-1.elb.amazonaws.com'
+    ]
+  }
 })
+
